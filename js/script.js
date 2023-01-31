a810844339a612a4b46275a698be24e13a50a74f
Riaz Khan <37626809+khanriazaoif@users.noreply.github.com>
2023-01-30 20:17:27 -0600

    Final Project v2

--- js/script.js
+++ js/script.js
@@ -18,6 +18,8 @@ Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
+//meets expectations request
+
 function showPage( list, page) {
     let startIndex = (page * 9) - 9;
     let endIndex = (page * 9);
